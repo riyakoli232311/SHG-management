f8f8adef40776eaa7a309c1e3317e2967c5b5163
riyakoli232311 <riyakoli232311@gmail.com>
2026-03-18 21:26:24 +0530

    moreee

--- server/read_err.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-try {
-  const content = fs.readFileSync('error.log', 'utf8');
-  console.log('--- ERROR LOG HEAD ---');
-  console.log(content.substring(0, 500));
-  console.log('--- ERROR LOG TAIL ---');
-  console.log(content.slice(-2000));
-} catch(e) { console.error(e); }
